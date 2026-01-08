8f9fc39488f2126d11ab545662873d7326d7d574
Aswinthmani V <122224704+Aswinthmani2003@users.noreply.github.com>
2026-01-08 23:23:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string
+import os
 
 app = Flask(__name__)
 
@@ -1955,5 +1956,6 @@ from WhatsApp Cloud APIs to CRM-driven decision pipelines.
 def home():
     return render_template_string(template)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
